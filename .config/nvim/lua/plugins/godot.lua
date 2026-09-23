613a92994a10a734aa89a50f3f92e3521d6f5be8
Kevin O'Neil <kevinoneil@Kevins-MacBook-Pro.local>
2026-09-23 02:25:35 -0700

    Updates godot lua plugin

--- .config/nvim/lua/plugins/godot.lua
+++ .config/nvim/lua/plugins/godot.lua
@@ -1,5 +1,30 @@
 return {
   "lommix/godot.nvim",
-  lazy = true,
+  -- DAP configuration
+  dap = {
+    host = "127.0.0.1",
+    port = 6006,
+  },
+
   cmd = { "GodotDebug", "GodotBreakAtCursor", "GodotStep", "GodotQuit", "GodotContinue" },
+
+  -- Path to your Godot executable
+  bin = "godot",
+
+  -- GUI settings for console (passed to nvim_open_win)
+  gui = {
+    console_config = {
+      anchor = "SW",
+      border = "double",
+      col = 1,
+      height = 10,
+      relative = "editor",
+      row = 99999,
+      style = "minimal",
+      width = 99999,
+    },
+
+    -- Expose user commands automatically (optional)
+    expose_commands = true,
+  },
 }
